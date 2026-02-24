8e5822033f758a836b2d501ae28c3b04957700cf
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-02-24 17:13:08 -0300

    feat exercicios com List

---
+++ Exercicios/GerenciarCapacidadeLists/GerenciarCapacidadeLists/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciarCapacidadeLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numeros = new List<int>(10) { 1, 2, 3, 4, 5 };
+
+            MostrarInformacoesLista(numeros);
+
+            numeros.Clear();
+
+            MostrarInformacoesLista(numeros);
+
+            numeros.TrimExcess();
+
+            MostrarInformacoesLista(numeros);
+        }
+        static void MostrarInformacoesLista(List<int> lista)
+        {
+            Console.WriteLine(
+                $"Count: {lista.Count}\n" +
+                $"Capacity: {lista.Capacity}"
+                );
+        }
+    }
+}
