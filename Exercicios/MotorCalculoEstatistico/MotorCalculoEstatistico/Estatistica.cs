73fb7f6ce7a7e7111143aaa4d9df532bfe28332e
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-02-25 17:19:05 -0300

    feat organizacao de pastas

--- Exercicios/MotorCalculoEstatistico/MotorCalculoEstatistico/Estatistica.cs
+++ Exercicios/MotorCalculoEstatistico/MotorCalculoEstatistico/Estatistica.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace MotorCalculoEstatistico
 {
-    internal class Estatistica
+    public static class Estatistica
     {
+        private static void LancarErroListaInvalida()
+        {
+            throw new ArgumentException("Lista inválida.");
+        }
+        public static double CalcularMedia(double a, double b)
+        {
+            return CalcularMedia(new double[] { a, b });
+        }
+        public static double CalcularMedia(double a, double b, double c)
+        {
+            return CalcularMedia(new double[] { a, b, c });
+        }
+        public static double CalcularMedia(List<double> valores)
+        {
+            return CalcularMedia(valores.ToArray());
+        }
+
+        public static double CalcularMedia(IEnumerable<double> valores)
+        {
+            var lista = valores.ToList();
+            if (valores == null || lista.Count <= 0)
+            {
+                LancarErroListaInvalida();
+            }
+            return lista.Average();
+        }
+
+        public static double CalcularMedia(double[] valores, bool ignorarNegativos)
+        {
+            if (ignorarNegativos)
+            {
+                return CalcularMedia(valores.Where(num => num >= 0).ToArray());
+            }
+            else 
+            {
+                return CalcularMedia(valores);
+            }
+        }
     }
 }
