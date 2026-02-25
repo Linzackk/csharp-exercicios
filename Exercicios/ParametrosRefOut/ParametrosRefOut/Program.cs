5fe24c535dfdd7099ab765c1d9d2e40f576fb3b9
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-02-25 14:33:40 -0300

    feat exercicios com Metodos

---
+++ Exercicios/ParametrosRefOut/ParametrosRefOut/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametrosRefOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidadeMacas = 53;
+            int macasVendidas = 17;
+            AtualizarEstoque(ref quantidadeMacas, macasVendidas);
+            Console.WriteLine($"Estoque atual: {quantidadeMacas}\n");
+
+            float nota1 = 5.9F;
+            float nota2 = 7.2F;
+
+            float media = CalcularMedia(nota1, nota2, out bool aprovado);
+            Console.WriteLine($"Média: {media} - {aprovado}");
+        }
+
+        static void AtualizarEstoque(ref int estoqueAtual, int quantidadeVendida)
+        {
+            estoqueAtual -= quantidadeVendida;
+        }
+
+        static float CalcularMedia(float nota1, float nota2, out bool aprovado)
+        {
+            float media = (nota1 + nota2) / 2;
+            if (media >= 6) { aprovado = true; }
+            else { aprovado = false; }
+            return media;
+        }
+    }
+}
---
+++ Exercicios/SobrecargaMetodos/SobrecargaMetodos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaMetodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 7;
+            int c = 2;
+
+            double da = 8.1D;
+            double db = 9.2D;
+
+            var result1 = Calculadora.Multiplicar(a, b);
+            var result2 = Calculadora.Multiplicar(da, db);
+            var result3 = Calculadora.Multiplicar(a, b, c);
+
+            Console.WriteLine($"Resultados: \n{result1}\n{result2}\n{result3}");
+        }
+    }
+
+    class Calculadora
+    {
+        static public int Multiplicar(int a, int b) { return a * b; }
+        static public int Multiplicar(int a, int b, int c) { return a * b * c; }
+        static public double Multiplicar(double a, double b) { return a * b; }
+    }
+}
