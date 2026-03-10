10ab86d64205ba0bd4432981131bd72358c3f4ac
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-03-10 16:53:58 -0300

    feat exercicio de controller de api

---
+++ API/PrimeiraApi/PrimeiraApi/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApi.Models;
+
+namespace PrimeiraApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok("Lista de Produtos");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            if (id <= 0)
+                return NotFound("Produto não encontrado");
+
+            return Ok("Produto com ID " + id);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            if (product == null)
+                return BadRequest("Produto inválido.");
+
+            return CreatedAtAction(nameof(GetProductById), new {id = product.Id}, product);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            if (id <= 0)
+                return BadRequest("Id Inválido");
+
+            return NoContent();
+        }
+    }
+}
---
+++ API/PrimeiraApi/PrimeiraApi/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace PrimeiraApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
