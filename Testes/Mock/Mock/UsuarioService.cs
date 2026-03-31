2791c68abb390caf5bd899a682ad37943517ffee
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-03-31 14:16:36 -0300

    feat testes com Moq

---
+++ Testes/Mock/Mock/UsuarioService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mock
+{
+    public class UsuarioService
+    {
+        public readonly IUsuarioRepository _repository;
+
+        public UsuarioService(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Usuario? ObterUsuario(int id)
+        {
+            var usuario = _repository.BuscarPorId(id);
+
+            if (usuario == null)
+                throw new Exception("Usuario não encontrado.");
+
+            return usuario;
+        }
+    }
+}
