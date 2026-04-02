e1e76c10e042090e0c1d56a52711c5634a5c6ed9
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-02 15:10:39 -0300

    feat uso de Clean Code e Refatoração

---
+++ Testes/CleanCode/CleanCode/Refatoracao/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode.Refatoracao
+{
+    public class Item
+    {
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public double Preco { get; set; }
+
+    }
+}
